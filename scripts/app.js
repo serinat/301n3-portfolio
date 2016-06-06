1516fb762fa6f34c290c2b8162de4543b647680e
Serina Tsang <serinat@uw.edu>
2016-06-05 23:34:53 -0700

    printing to DOM but still have more work to do

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+var projects = [];
+
+function Project(projectObj) {
+  this.title = projectObj.title;
+  this.publishedOn = projectObj.publishedOn;
+  this.body = projectObj.body;
+}
+
+Project.prototype.toHtml = function () {
+  var $newProject = $('project.template').clone();
+
+  $newProject.find('h2').text(this.title);
+  $newProject.find('time').text(this.publishedOn);
+  $newProject.find('.project-body').html(this.body);
+
+  $newProject.append('<hr>');
+
+  $newProject.removeClass('template');
+
+  return $newProject;
+}
+
+projectData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+projectData.forEach(function(ele) {
+  projects.push(new Project(ele));
+})
+
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml())
+});
