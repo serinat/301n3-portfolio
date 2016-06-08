cb487b0e139bf4dd5517f00c3d540d55a008820a
Serina Tsang <serinat@uw.edu>
2016-06-08 00:24:19 -0700

    added contact links and worked on styling

---
+++ scripts/projectView.js
@@ -0,0 +1,14 @@
+var projectView = {};
+
+projectView.handleMainNav = function() {
+  $('.main-nav').on('click', '.tab', function(e) {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+
+  $('.main-nav .tab:first').click();
+};
+
+$(document).ready(function() {
+  projectView.handleMainNav();
+});
