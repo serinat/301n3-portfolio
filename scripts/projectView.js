89df769f0294500b4ffed26f1a236337bc2d611e
Serina Tsang <serinat@uw.edu>
2016-06-11 00:16:38 -0700

    added handlebars, SMACCS and filters but one filter isn't working yet

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -3,15 +3,60 @@ var projectView = {};
 projectView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function() {
     $('.tab-content').hide();
-    console.log($(this).data('content'));
+    // console.log($(this).data('content'));
     $('#' + $(this).data('content')).fadeIn();
   });
 
   $('.main-nav .tab:first').click();
 };
 
+projectView.populateFilters = function() {
+  $('project').each(function() {
+    if (!$(this).hasClass('template')) {
+      var val = $(this).find('h2').text();
+      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      $('#project-filter').append(optionTag);
+
+      val = $(this).attr('data-category');
+      optionTag = '<option value="' + val + '">' + val + '</option>';
+      if ($('#category-filter option[value="' + val + '"]').length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+projectView.handleProjectFilter = function() {
+  $('#project-filter').on('change', function() {
+    if ($(this).val()) {
+      $('project').hide();
+      $('project[data-project="' + $(this).val() + '"]').fadeIn();
+    } else {
+      $('project').fadeIn();
+      $('project.template').hide();
+    }
+    $('#category-filter').val('');
+  });
+};
+
+projectView.handleCategoryFilter = function() {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('project').hide();
+      $('project[data-category="' + $(this).val() + '"]').fadeIn();
+    } else {
+      $('project').fadeIn();
+      $('project.template').hide();
+    }
+    $('#project-filter').val('');
+  });
+};
+
 $(document).ready(function() {
   $(projectView.handleMainNav());
+  $(projectView.populateFilters());
+  $(projectView.handleCategoryFilter());
+  $(projectView.handleProjectFilter());
 });
 // $('.main-nav').on('click', '.tab', function(e) {
 //   e.preventDefault();
--- scripts/projects.js
+++ scripts/projects.js
@@ -2,10 +2,13 @@ var projectData = [
   {
     title: 'First Thursday Gallery Finder',
     publishedOn: '2016-02-25',
-    body: '<p>An interactive map using geolocation, gallery details, and searchbox for art walk visitors. Check it out <a href=\'http://lillianszugyi.github.io/art-gallery-finder/\'>here</a></p>'
+    body: '<p>An interactive map using geolocation, gallery details, and searchbox for art walk visitors. Check it out <a href=\'http://lillianszugyi.github.io/art-gallery-finder/\'>here</a></p>',
+    category: 'Art'
   },
-  { title: 'Pat\'s Cookie-Stand',
+  {
+    title: 'Pat\'s Cookie-Stand',
     publishedOn: '2016-02-07',
-    body: '<p>First real-world application website for Pat\'s salmon cookie shops. Check it out <a href =\'http://serinat.github.io/cookie-stand/\'>here</a></p>'
-  },
+    body: '<p>First real-world application website for Pat\'s salmon cookie shops. Check it out <a href =\'http://serinat.github.io/cookie-stand/\'>here</a></p>',
+    category: 'Food'
+  }
 ];
